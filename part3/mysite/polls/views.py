192c81f96d2551dacfbe6ee91c669b44ae9a34ad
Toshiyuki Ogura <tos.ogura@gmail.com>
2014-07-29 15:40:28 +0900

    index view that lists polls (page's design is hard-coded)

--- part3/mysite/polls/views.py
+++ part3/mysite/polls/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
