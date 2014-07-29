bb080a1dd721d8cdd83c2596eabf8a2d375558d5
Toshiyuki Ogura <tos.ogura@gmail.com>
2014-07-29 15:50:56 +0900

    shortcut (render)

--- part3/mysite/polls/views.py
+++ part3/mysite/polls/views.py
@@ -5,11 +5,8 @@ from polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_poll_list': latest_poll_list,
-        })
-    return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
