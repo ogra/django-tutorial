b6d20245dfb5c4af02fa7b5b57b0c59f02a0d43c
Toshiyuki Ogura <tos.ogura@gmail.com>
2014-07-29 15:54:46 +0900

    detail view (raise 404 error)

--- part3/mysite/polls/views.py
+++ part3/mysite/polls/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
-from django.template import RequestContext, loader
 from polls.models import Poll
 
 def index(request):
@@ -9,7 +8,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
