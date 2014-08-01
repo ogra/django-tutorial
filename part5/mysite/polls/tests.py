4c0d054f470e208cfb3efb7c50770e4b4660e459
Toshiyuki Ogura <tos.ogura@gmail.com>
2014-08-01 15:00:29 +0900

    more comprehensive tests

--- part5/mysite/polls/tests.py
+++ part5/mysite/polls/tests.py
@@ -12,3 +12,20 @@ class PollMethodTests(TestCase):
         """
         future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
         self.assertEqual(future_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is older than 1 day
+        """
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_recent_poll(self):
+        """
+        was_published_recently() should return True for polls whose
+        pub_date is withinthe last day
+        """
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
