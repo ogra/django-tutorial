6a09f8cc3595c4f90bcb7e741d3747af500321e0
Toshiyuki Ogura <tos.ogura@gmail.com>
2014-08-02 11:24:16 +0900

    improve index view (not to display future polls)

--- part5/mysite/polls/views.py
+++ part5/mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.views import generic
 from polls.models import Choice, Poll
 
@@ -9,8 +10,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published polls
+        (not including those set to be published
+        in the future).
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
