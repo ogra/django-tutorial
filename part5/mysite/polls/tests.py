53f6b2dee65edacfe858eea5874ef451e333b6be
Toshiyuki Ogura <tos.ogura@gmail.com>
2014-08-02 13:39:15 +0900

    test new view (index)

--- part5/mysite/polls/tests.py
+++ part5/mysite/polls/tests.py
@@ -1,7 +1,11 @@
 import datetime
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 from polls.models import Poll
+
+def create_poll(question, days):
+    return Poll.objects.create(question=question, pub_date=timezone.now() + datetime.timedelta(days=days))
 
 class PollMethodTests(TestCase):
 
@@ -29,3 +33,41 @@ class PollMethodTests(TestCase):
         """
         recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
         self.assertEqual(recent_poll.was_published_recently(), True)
+
+
+class PollViewTests(TestCase):
+
+    def test_index_view_with_no_polls(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+
+    def test_index_view_with_a_past_poll(self):
+        create_poll(question="Past poll.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+                response.context['latest_poll_list'],
+                ['<Poll: Past poll.>'])
+
+    def test_index_view_with_a_future_poll(self):
+        create_poll(question="Future poll.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.", status_code=200)
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+
+    def test_index_view_with_future_poll_and_past_poll(self):
+        create_poll(question="Past poll.", days=-30)
+        create_poll(question="Future poll.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+                response.context['latest_poll_list'],
+                ['<Poll: Past poll.>'])
+
+    def test_index_view_with_two_past_polls(self):
+        create_poll(question="Past poll 1.", days=-30)
+        create_poll(question="Past poll 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+                response.context['latest_poll_list'],
+                ['<Poll: Past poll 2.>', '<Poll: Past poll 1.>'])
